fc5a18f982ef50a7f4d7efe838c4746b9c67a254
dutycaws <ho.steven.m@gmail.com>
2026-03-18 09:37:11 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it   -v /home/hosm/Projects/security-policy-builder/output:/output   --rm jupiterone/pspbuilder   psp build   -c /output/templates/config.json   -o /output/docs   -p /output/partials   -s /output/templates
+docker run -it   -v /home/hosm/Documents/output:/output   --rm jupiterone/pspbuilder   psp build   -c /output/templates/config.json   -o /output/docs   -p /output/partials   -s /output/templates
