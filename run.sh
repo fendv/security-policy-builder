25384fc9d449aee144a5145782ba8d5a99945959
dutycaws <ho.steven.m@gmail.com>
2026-03-17 21:19:50 -0500

    Convenience

---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run -it   -v /home/hosm/Projects/security-policy-builder/output:/output   --rm jupiterone/pspbuilder   psp build   -c /output/templates/config.json   -o /output/docs   -p /output/partials   -s /output/templates
